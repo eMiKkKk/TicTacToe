844dd35eab62209141a87bc4c96d073a550f32bf
alexei57 <vdovichenko.alexei@gmail.com>
2026-03-30 19:05:56 +0300

    feat: add function startGame and switchPlayer

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const board = document.querySelector('.playtable');
 const cell = document.querySelector('.playcell');
 const newGameButton = document.querySelector('.button_newgame');
-let currentPlayer = 'X';
+let movePlayer = document.querySelector('.move');
+let players = ['X', '0'];
+let currentPlayerIndex;
+
+function startGame() {
+  currentPlayerIndex = Math.floor(Math.random() * players.length);
+  movePlayer.textContent = `Ход игрока: ${players[currentPlayerIndex]}`;
+}
+
+startGame();
 
 board.addEventListener('click', (evt) => {
 
@@ -9,12 +18,24 @@ if (evt.target.dataset.blocked == 'true') {
   alert('Уже сходили сюда, кретин')
 }
 else {
-  evt.target.innerText = currentPlayer;
+  evt.target.textContent = players[currentPlayerIndex];
+  switchPlayer();
 }
 evt.target.dataset.blocked = 'true';
-console.log(evt.target.textcontent);
+console.log(evt.target.textContent);
 
 
 });
 
 newGameButton.addEventListener('click', () => console.clear())
+
+function switchPlayer() {
+
+  if (currentPlayerIndex === 0) {
+    currentPlayerIndex = 1;
+  } else {
+    currentPlayerIndex = 0;
+  }
+
+  movePlayer.textContent = `Ход игрока: ${players[currentPlayerIndex]}`;
+}
